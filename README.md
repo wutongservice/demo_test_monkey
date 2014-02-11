9e37ea97917c93b8ef2b4c82599ecff5982db608
Xiaocong He <xiaocong@gmail.com>
2014-02-11 17:49:54 +0800

    add monkey.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-demo_test_shell
-===============
+demo test monkey
+================
---
+++ monkey.py
@@ -0,0 +1,25 @@
+import subprocess
+import re
+
+def list3rdPartyPackages():
+    prco = subprocess.Popen(['adb', 'shell', 'pm', 'list', 'packages', '-3'], stdout=subprocess.PIPE)
+    out = prco.communicate()[0].decode('utf-8')
+    packages = []
+    for line in out.splitlines():
+        m = re.match(r"^package:(\w+(?:\.\w+)*)", line)
+        if m:
+            packages.append(m.group(1))
+    return packages
+
+
+def main():
+    for package in list3rdPartyPackages():
+        print("Monkey test package %s" % package)
+        proc = subprocess.Popen(['adb', 'shell', 'monkey', '-p', package, '100'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc.wait()
+        if proc.returncode != 0:
+            exit(-1)
+
+
+if __name__ == '__main__':
+    main()
